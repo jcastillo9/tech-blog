7ec203aed71927b7577752e4d44dc0ba6647b0fa
Janette <janette.castillo91@yahoo.com>
2022-01-09 23:36:48 -0600

    added date created to seed data

--- seed/commentData.js
+++ seed/commentData.js
@@ -4,17 +4,20 @@ const commentData = [
     {
         comment_content: 'I learned this is class today!',
         user_id: '1',
-        post_id: '1'
+        post_id: '1',
+        date_created: '12/25/2021'
     },
     {
         comment_content: 'I am having a hard time understanding concepts but I enjoy working with my classmates.',
         user_id: '2',
-        post_id: '2'
+        post_id: '2',
+        date_created: '10/02/2021'
     },
     {
         comment_content: 'Can you go into this a little more?',
         user_id: '3',
-        post_id: '3'
+        post_id: '3',
+        date_created: '09/25/2021'
     }
 ]
 
--- seed/postData.js
+++ seed/postData.js
@@ -4,17 +4,20 @@ const postData = [
     {
         title: 'Why is MVC so important?',
         content: 'MVC is important to understand because it is the basic structure which most web applications are built on. The same is also true for mobile apps and desktop programs.',
-        user_id: '1'
+        user_id: '1',
+        date_created: '10/25/2021'
     },
     {
         title: 'Bootcamp Experience',
         content: 'So far so good. Looking forward to starting Project 2',
-        user_id: '2'
+        user_id: '2',
+        date_created: '09/02/2021'
     },
     {
         title: 'Authenticaion v Authorization',
         content: 'There is  difference between authentication and authorization. Authetication means confirming your own identity, whereas authorization means being allowed access to the system.',
-        user_id: '3'
+        user_id: '3',
+        date_created: '08/22/2021'
     }
 ];
 

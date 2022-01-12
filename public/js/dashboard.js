1de18b0105b92c6af98d9e9b3716384902703a70
Janette <janette.castillo91@yahoo.com>
2022-01-12 00:32:30 -0600

    debugged dashboard page

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -35,10 +35,29 @@ const updateButtonHandler = async (event) => {
     }
 };
 
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to delete post');
+    }
+};
+
 document
-.querySelector('.new-post-form')
+.querySelector('#new-post-form')
 .addEventListener('submit', newPostForm);
 
 document
-.querySelector('.post-list')
+.querySelector('.updatePost')
 .addEventListener('submit', updateButtonHandler);
+
+document
+.querySelector('.deletePost')
+.addEventListener('submit', delButtonHandler);
--- public/js/login.js
+++ public/js/login.js
@@ -24,7 +24,7 @@ const signupForm = async (event) => {
 
     const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup');
+    const password = document.querySelector('#password-signup').value.trim();
 
     if (name && email && password) {
         const response = await fetch('/api/users', {
